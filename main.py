900f27fc9428732fb91a575be7a7714de4e20487
justabloob <justabloob@gmail.com>
2025-02-01 12:52:12 +0100

    finished game loop + .gitignore adeed

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ def main():
     print("Screen width: " + str(constants.SCREEN_WIDTH))
     print("Screen height: " + str(constants.SCREEN_HEIGHT))
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    game_loop(screen)
 
-def game_loop():
+def game_loop(screen):
     while True:
-        pygame.Surface.fill((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, 0))
+        screen.fill((0, 0, 0))
         pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
 
 if __name__ == "__main__":
     main()
