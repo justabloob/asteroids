b979367a54a0832ee6486984feb5541cbd4288c6
justabloob <justabloob@gmail.com>
2025-02-03 17:26:22 +0100

    added left and right rotation

--- main.py
+++ main.py
@@ -2,26 +2,25 @@ import pygame # type: ignore
 import constants
 from player import Player
 
-clock = pygame.time.Clock() # Creates a new clock object to track time
-dt = 0  # initialize delta time to 0 seconds
-player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
-
 def main():
     pygame.init()
     print("Starting asteroids!")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    game_loop(screen)
-
-def game_loop(screen):
+    clock = pygame.time.Clock() # Creates a new clock object to track time
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    dt = 0  # initialize delta time to 0 seconds
+    
     while True:
-        screen.fill((0, 0, 0))
-        player.draw(screen)
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-        dt = clock.tick(60) / 1000
+            
+        screen.fill((0, 0, 0))
+        player.draw(screen)
+        player.update(dt)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000 # limit the framerate to 60 FPS
 
 if __name__ == "__main__":
     main()
